227788529807eb5d2682a734228d88a636b9002c
me.hoseini <me.hoseini@ravanertebat.com>
2023-08-10 10:50:22 +0330

    secure elastic with loggie

--- loggie/start.sh
+++ loggie/start.sh
@@ -9,6 +9,7 @@ echo app is running.
 
 else
 
+docker cp premium-data_es01_1:/usr/share/elasticsearch/config/certs .
 nohup  ./loggie -config.pipeline "pipelines.local.yml" 2>&1 >> app.log &
 echo $! > app.pid
 echo app is started.
